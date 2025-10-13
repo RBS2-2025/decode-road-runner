df608ba9783c02a8244f0e2180bf94602d4ec267
dahoon-lee <dahoon0502@naver.com>
2025-10-13 10:29:01 +0900

    delaytest-code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/DelayTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/DelayTest.java
@@ -2,41 +2,65 @@ package org.firstinspires.ftc.teamcode.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-@TeleOp()
+@TeleOp(name="DelayTest")
 public class DelayTest extends LinearOpMode {
-    NormalizedColorSensor c1,c2,c3;
+
+    NormalizedColorSensor c1, c2, c3;
+    private DcMotor Dc;
+
 
     public boolean findWhite(NormalizedColorSensor sensor){
-
         NormalizedRGBA colors = sensor.getNormalizedColors();
 
-        float normRed, normBlue, normGreen;
-        normRed = colors.red/colors.alpha;
-        normBlue = colors.blue/colors.alpha;
-        normGreen = colors.green/colors.alpha;
+        float normRed = colors.red / colors.alpha;
+        float normGreen = colors.green / colors.alpha;
+        float normBlue = colors.blue / colors.alpha;
 
-        telemetry.addData("red",normRed);
-        telemetry.addData("green",normGreen);
-        telemetry.addData("blue",normBlue);
+        telemetry.addData("R", normRed);
+        telemetry.addData("G", normGreen);
+        telemetry.addData("B", normBlue);
 
         return normRed > 0.9 && normGreen > 0.9 && normBlue > 0.9;
     }
 
     @Override
-    public void runOpMode()  {
+    public void runOpMode() {
+        Dc = hardwareMap.get(DcMotor.class, "Dc");
+        c1 = hardwareMap.get(NormalizedColorSensor.class, "c1");
+        c2 = hardwareMap.get(NormalizedColorSensor.class, "c2");
+        c3 = hardwareMap.get(NormalizedColorSensor.class, "c3");
 
-        c1 = hardwareMap.get(NormalizedColorSensor.class,"c1");
-        c2 = hardwareMap.get(NormalizedColorSensor.class,"c2");
-        c3 = hardwareMap.get(NormalizedColorSensor.class,"c3");
-        if(opModeIsActive()){
-            telemetry.addData("c1",findWhite(c1));
-            telemetry.addData("c2",findWhite(c2));
-            telemetry.addData("c3",findWhite(c3));
+        telemetry.addLine("Ready to start");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            boolean w1 = findWhite(c1);
+            boolean w2 = findWhite(c2);
+            boolean w3 = findWhite(c3);
+
+            telemetry.addData("c1 white?", w1);
+            telemetry.addData("c2 white?", w2);
+            telemetry.addData("c3 white?", w3);
+
+
+            if (w1 && w2 && w3) {
+                Dc.setPower(1.0);
+
+            } else if(w1 || w2 || w3) {
+
+                Dc.setPower(-1);
+
+            } else {
+                Dc.setPower(0);
+            }
+
+            telemetry.update();
         }
     }
 }
