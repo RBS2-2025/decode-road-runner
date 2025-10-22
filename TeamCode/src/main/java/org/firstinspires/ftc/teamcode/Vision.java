22add451a273e4528d832c068f04e8b925c46425
imxxge <naver_vs@kakao.com>
2025-10-22 10:56:04 +0900

    get april tag id

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -4,33 +4,63 @@ import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+import java.util.List;
 
 @TeleOp(name = "Vision")
+@Disabled
 public class Vision extends LinearOpMode {
-    Limelight3A limelight;
+
+    private Limelight3A limelight;
 
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException
+    {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
 
-        limelight.pipelineSwitch(0); // april tag 인식 파이프라인
+        telemetry.setMsTransmissionInterval(11);
+
+        limelight.pipelineSwitch(0);
+
+        /*
+         * Starts polling for data.  If you neglect to call start(), getLatestResult() will return null.
+         */
         limelight.start();
+
+        telemetry.addData(">", "Robot Ready.  Press Play.");
+        telemetry.update();
         waitForStart();
 
-        if (opModeIsActive()) {
-            while (opModeIsActive()) {
-                LLResult result = limelight.getLatestResult();
-                if (result != null && result.isValid()) {
-                    double tx = result.getTx();
-                    double ty = result.getTy();
+        while (opModeIsActive()) {
+            LLResult result = limelight.getLatestResult();
+            if (result.isValid()) {
+                // Access general information
+                Pose3D botpose = result.getBotpose();
 
-                    telemetry.addData("Target X", tx);
-                    telemetry.addData("Target Y", ty);
-                } else {
-                    telemetry.addData("Limelight", "No Targets");
+                telemetry.addData("tx", result.getTx());
+                telemetry.addData("txnc", result.getTxNC());
+                telemetry.addData("ty", result.getTy());
+                telemetry.addData("tync", result.getTyNC());
+
+                telemetry.addData("Botpose", botpose.toString());
+
+                // Access fiducial results 아마도 april tag id 가져올 수 있음
+                List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+                for (LLResultTypes.FiducialResult fr : fiducialResults) {
+                    telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(), fr.getTargetXDegrees(), fr.getTargetYDegrees());
                 }
+
+            } else {
+                telemetry.addData("Limelight", "No data available");
             }
+
+            telemetry.update();
         }
+        limelight.stop();
     }
 }
